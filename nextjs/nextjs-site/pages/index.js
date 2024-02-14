13c0c949d250173d950ac06d6aaabd1950977064
Sarah Rudder <sarah.rudder@gsa.gov>
2024-02-14 14:48:36 -0600

    Added useState and useEffect example

--- nextjs/nextjs-site/pages/index.js
+++ nextjs/nextjs-site/pages/index.js
@@ -1,14 +1,28 @@
-import { Suspense } from 'react';
+import { Suspense, useEffect, useState } from 'react';
+
 import styles from '../styles/Home.module.css';
 import Link from 'next/link';
 import { getUsers } from '../api/users';
 
-async function Users() {
-    const users = await getUsers();
+function Users() {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        let fetchHappened = false;
+        const fetchUsers = async () => {
+            try {
+                const fetchedResult = await getUsers();
+                if (!fetchHappened) setUsers(fetchedResult);
+            } catch (error) {
+                console.log(error)
+            }
+            fetchHappened = true;
+        }
+        fetchUsers();
+    })
 
     return (
-        <ul>{ users.map(user => (
-            <li key={user.id}>{ user.name }</li>
+        <ul>{users.map(user => (
+            <li key={user.id}>{user.name}</li>
         ))}</ul>
     )
 }
