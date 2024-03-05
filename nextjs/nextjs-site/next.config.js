d5d7938ffcb2f770096866b315ea9953c7b1e33c
Jessica Dussault <jessica.dussault@gsa.gov>
2024-03-05 15:10:20 -0600

    adds build job to existing pr action

--- nextjs/nextjs-site/next.config.js
+++ nextjs/nextjs-site/next.config.js
@@ -3,5 +3,9 @@ module.exports = {
     // directories that eslint will run on
     // you can also list files (must give the file extension)
     dirs: ['api', 'app', 'pages', '__tests__', 'middleware.js']
+  },
+  generateBuildId: async () => {
+    // this is a placeholder
+    return "0.0.1";
   }
 }
