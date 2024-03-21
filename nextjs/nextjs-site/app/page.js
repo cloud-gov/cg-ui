540514adcfc471b652803d6c66053a405cf5fcc9
Eleni Chappen <eleni.chappen@gsa.gov>
2024-03-21 14:09:29 -0500

    initial USWDS setup

--- nextjs/nextjs-site/app/page.js
+++ nextjs/nextjs-site/app/page.js
@@ -9,6 +9,7 @@ export default function Home() {
             <li><Link href="/serverside">Example of serverside rendering</Link></li>
             <li><Link href="/users">Example of dynamic rendering (server-side)</Link></li>
             <li><Link href="/cloudfoundry">Cloudfoundry home</Link></li>
+            <li><Link href="/design-guide">Design guide</Link></li>
         </ul>
         </div>
     );
