189d2ee61e7df5409c252669fadff32c5b831063
Jessica Dussault <jessica.dussault@gsa.gov>
2024-03-28 09:48:37 -0500

    adds tests for list display

---
+++ nextjs/nextjs-site/__tests__/app/session/list.test.js
@@ -0,0 +1,33 @@
+/**
+ * @jest-environment jsdom
+ */
+import {
+    describe, expect, it
+}                           from '@jest/globals';
+import                      '@testing-library/jest-dom';
+import { render, screen }   from '@testing-library/react';
+import { SessionList }      from '../../../app/session/list';
+
+describe('SessionList', () => {
+    it('renders an empty list if no sessions are provided', async () => {
+        render(<SessionList sessions={[]}/>);
+        expect(
+            await screen.findByText('Active sessions')
+        ).toBeInTheDocument();
+        expect(
+            await screen.getByRole('list')
+        ).toBeEmptyDOMElement();
+    });
+
+    it('renders a list if sessions are provided', async () => {
+        render(<SessionList sessions={
+            [{id: 1, username: 'Test1'}, {id: 2, username: 'Test2'}]
+        } />);
+
+        expect(
+            await screen.getAllByRole('listitem')
+        ).toHaveLength(2);
+        expect(await screen.findByText('Test1')).toBeInTheDocument();
+        expect(await screen.findByText('Test2')).toBeInTheDocument();
+    });
+});
