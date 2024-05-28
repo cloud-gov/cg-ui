855b7edbd3bdbac7e203ee4d96dd4ccc26828eac
Brian Hurst <brian.hurst@gsa.gov>
2024-05-28 11:38:09 -0400

    Sticky header

--- app/layout.js
+++ app/layout.js
@@ -15,21 +15,24 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className="bg-accent-warm-light">
-        <Banner />
-        <div className="grid-container">
-          <div className="grid-row">
-            <div className="tablet:grid-col-2">
-              <div className="margin-y-4 tablet:margin-y-6 text-center">
-                <Image priority src={cloudGovIcon} alt="cloud.gov" />
-              </div>
+    <html lang="en" className="height-full">
+      <body className="bg-accent-warm-light height-full">
+        <div className="border-bottom border-white">
+          <Banner />
+        </div>
+        <div className="grid-container height-full desktop:display-flex overflow-hidden">
+          <div className="desktop:grid-col-2">
+            <div className="margin-y-4 text-center">
+              <Image priority src={cloudGovIcon} alt="cloud.gov" width="160" />
             </div>
-            <div className="tablet:grid-col-10 bg-white">
-              <div className="grid-container">
-                <main className="padding-y-4">{children}</main>
-              </div>
+          </div>
+          <div className="desktop:grid-col-10 bg-white height-full flex-column display-flex">
+            <div className="padding-x-4 display-flex border-bottom border-accent-warm-light shrink">
+              <span className="font-ui-xl padding-y-2 text-bold">
+                Org : 18F Stratos rebuild
+              </span>
             </div>
+            <main className="padding-4 overflow-y-auto grow">{children}</main>
           </div>
         </div>
         <Footer />
