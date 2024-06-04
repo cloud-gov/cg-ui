79db86d86fe471f4ed4714f9e32c1676bb78d2c7
Eleni Chappen <eleni.chappen@gsa.gov>
2024-06-04 08:46:58 -0500

    fix: remove redundant classes from Button component

--- components/UsersList/UsersListUsername.tsx
+++ components/UsersList/UsersListUsername.tsx
@@ -6,10 +6,7 @@ export function UsersListUsername({ username }: { username: string }) {
   return (
     <div className="margin-bottom-2 tablet:margin-bottom-0">
       <h3 className="margin-bottom-0 text-break-all">{username}</h3>
-      <Button
-        unstyled
-        className="usa-button usa-button--unstyled font-body-2xs"
-      >
+      <Button unstyled className="font-body-2xs">
         Remove from org
       </Button>
     </div>
