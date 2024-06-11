004a3c85373537294a6e85485daf4048e88a2bf0
Jessica Dussault <jessica.dussault@gsa.gov>
2024-06-11 14:33:56 -0500

    quick script to add users to our dev org for user testing purposes

---
+++ scripts/usability-test-round-1/script.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/env bash
+
+# Script that resets the users and roles for the development environment's
+# development org for user testing.
+#
+# Requirements:
+#   1. You must be logged into a CF CLI
+#   2. Set the values in `script.env`
+
+# load in variables ORG_NAME, ORG_MANAGERS, and ORG_AUDITORS
+directory_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$directory_path"
+source script.env
+
+echo "Resetting the users for ${ORG_NAME}"
+
+for i in "${ORG_MANAGERS[@]}"
+do
+  cf set-org-role "$i" "$ORG_NAME" OrgManager
+done
+
+for i in "${ORG_AUDITORS[@]}"
+do
+  cf set-org-role "$i" "$ORG_NAME" OrgAuditor
+done
