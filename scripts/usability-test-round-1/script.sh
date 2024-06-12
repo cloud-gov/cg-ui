0b2bc8d240b87330e546ca3607525d0c5a072f31
Jessica Dussault <jessica.dussault@gsa.gov>
2024-06-12 11:45:41 -0500

    Update scripts/usability-test-round-1/script.sh
    Co-authored-by: Eleni Chappen <eleni.chappen@gsa.gov>

--- scripts/usability-test-round-1/script.sh
+++ scripts/usability-test-round-1/script.sh
@@ -5,7 +5,7 @@
 #
 # Requirements:
 #   1. You must be logged into a CF CLI
-#   2. Set the values in `script.env`
+#   2. Set the values in `script.env` (users must be existing Cloud.gov users for specified environment)
 
 # load in variables ORG_NAME, ORG_MANAGERS, and ORG_AUDITORS
 directory_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
