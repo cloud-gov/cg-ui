afed500e0781bc035f99ba278757eb875cf9680a
Jessica Dussault <jessica.dussault@gsa.gov>
2024-08-14 11:47:52 -0500

    switches env variables to mock implementation tests
    Fix for misaligned hover states
    
    Since the Pages icon was 4px shorter than the Dashboard icon, it was causing the borders on hover/focus states to be misaligned. This updated logo fixes the issue!

--- __tests__/components/auth/Auth.js
+++ __tests__/components/auth/Auth.js
@@ -4,17 +4,27 @@
 import { describe, expect, it } from '@jest/globals';
 import { render, screen } from '@testing-library/react';
 import { Auth } from '@/components/auth/Auth';
+import { isLoggedIn } from '@/api/cf/token';
+
+/* global jest */
+/* eslint no-undef: "off" */
+jest.mock('@/api/cf/token', () => ({
+  isLoggedIn: jest.fn(),
+}));
+/* eslint no-undef: "error" */
 
 describe('Auth', () => {
   it('when not logged in, displays log in button', async () => {
-    delete process.env.CF_API_TOKEN;
+    isLoggedIn.mockReturnValue(false);
+
     render(<Auth />);
     const content = await screen.findByText('Log In');
     expect(content).toBeInTheDocument();
   });
 
   it('when logged in, displays log out button', async () => {
-    process.env.CF_API_TOKEN = 'placeholder';
+    isLoggedIn.mockReturnValue(true);
+
     render(<Auth />);
     const content = await screen.findByText('Log Out');
     expect(content).toBeInTheDocument();
