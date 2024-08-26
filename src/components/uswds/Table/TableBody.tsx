b861f5de6095634fc07cf25594681cd746274619
Eleni Chappen <eleni.chappen@gsa.gov>
2024-08-26 13:29:42 -0500

    keep smaller font size consistent in mobile

--- src/components/uswds/Table/TableBody.tsx
+++ src/components/uswds/Table/TableBody.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export function TableBody({ children }: { children: React.ReactNode }) {
-  return <tbody className="bg-white mobile-lg:font-ui-2xs">{children}</tbody>;
+  return <tbody className="bg-white font-ui-2xs">{children}</tbody>;
 }
