ab56c92c80b9e73a808a71e8bc92744104e8eb17
Eleni Chappen <eleni.chappen@gsa.gov>
2024-08-28 15:43:48 -0500

    refocus on last focused element when modal is closed

--- src/components/uswds/Modal.tsx
+++ src/components/uswds/Modal.tsx
@@ -23,7 +23,9 @@ export function Modal({
   useEffect(() => {
     if (!modalRef) return;
     const modalElement = modalRef.current;
-    if (!modalElement) return;
+    if (!modalElement) return; // modal is closed
+    // Form here on, modal is open
+    const lastFocusedElement = document.activeElement as HTMLElement;
 
     const focusableElements = (modalElement as HTMLElement).querySelectorAll(
       'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
@@ -56,6 +58,8 @@ export function Modal({
       // remove event listeners when component is unmounted
       window.removeEventListener('keydown', handleTabKeyPress);
       window.removeEventListener('keydown', handleEscapeKeyPress);
+      // return focus to last focused element before modal was opened
+      lastFocusedElement?.focus();
     };
   }, [close, modalRef]);
 
