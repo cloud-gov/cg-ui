c17b46fd6ff1ab801cc4237e1053bd44ad0da164
Ethan Marcotte <ethan.marcotte@gsa.gov>
2024-09-20 10:39:09 -0400

    header: alignment/height adjustments

--- src/components/NavGlobal/NavGlobalLinkProduct.tsx
+++ src/components/NavGlobal/NavGlobalLinkProduct.tsx
@@ -21,7 +21,8 @@ export function NavGlobalLinkProduct({
     },
     // USWDS classes
     'display-flex',
-    'margin-bottom-2px',
+    'padding-y-105',
+    'tablet:padding-y-2',
     'text-no-underline',
     'text-white',
     // USWDS applied only when active
