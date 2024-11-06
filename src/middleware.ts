b09fb3e234b73132ea1257dcf3b06f47d1b0b88f
Eleni Chappen <eleni.chappen@gsa.gov>
2024-11-06 13:28:15 -0600

    fix: ensure root path is always caught in middleware https://github.com/vercel/next.js/discussions/38615#discussioncomment-10625267

--- src/middleware.ts
+++ src/middleware.ts
@@ -20,18 +20,7 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    {
-      source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
-      missing: [
-        { type: 'header', key: 'next-router-prefetch' },
-        { type: 'header', key: 'purpose', value: 'prefetch' },
-      ],
-    },
-    /*
-     * Match Root
-     */
-    {
-      source: '/',
-    },
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    '/',
   ],
 };
