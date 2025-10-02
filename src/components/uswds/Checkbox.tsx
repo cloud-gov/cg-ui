ac8241e48f226d07b6a99750a70013e0fba36b24
Mark Boyd <mark.boyd@gsa.gov>
2025-10-02 17:39:44 -0400

    fix build issues

--- src/components/uswds/Checkbox.tsx
+++ src/components/uswds/Checkbox.tsx
@@ -26,7 +26,7 @@ export const Checkbox = ({
   tile,
   ...inputProps
   // eslint-disable-next-line no-undef
-}: CheckboxProps & JSX.IntrinsicElements['input']): React.ReactElement => {
+}: CheckboxProps & React.JSX.IntrinsicElements['input']): React.ReactElement => {
   const classes = classnames('usa-checkbox', className);
   const checkboxClasses = classnames('usa-checkbox__input', {
     'usa-checkbox__input--tile': tile,
